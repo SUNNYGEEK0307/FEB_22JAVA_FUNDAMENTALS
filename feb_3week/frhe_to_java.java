472629df5fdc6a08ce31b5c054495eb339bfda3a
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:31:26 +0530

    Update frhe_to_java.java

--- feb_3week/frhe_to_java.java
+++ feb_3week/frhe_to_java.java
@@ -1,5 +1,13 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class frhe_to_java {
-
+	public static void main(String[] args) {
+//		Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+		double fahrenheit, celsius;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Temperature fahrenheit: ");
+		fahrenheit=sc.nextDouble();
+		celsius = (fahrenheit-32)*5/9;
+		System.out.print(fahrenheit+" F = "+celsius+" C");
+	}
 }
