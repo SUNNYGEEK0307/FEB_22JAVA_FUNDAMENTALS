016c78e803ef7ca8f13b4238703f5474bfb58b6a
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:37:01 +0530

    Update square_of_sumupto_n.java

--- feb_3week/square_of_sumupto_n.java
+++ feb_3week/square_of_sumupto_n.java
@@ -1,5 +1,18 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class square_of_sumupto_n {
+	/* WAP that prints square of sum up to 10 example output : 1^2+ 2^2 + 3^2 + 4^2
+		+ 5^2 + ........... + 9^2 + 10^2*/
+public static void main(String[] args) {
+	int n, sum = 0;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter last number: ");
+	n = sc.nextInt();
+	for (int i = 1; i <= n; i++) {
+		sum = sum + i * i;
+	}
+	System.out.print(sum);
+}
 
 }
+
