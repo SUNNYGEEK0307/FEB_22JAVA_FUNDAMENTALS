23549423c81ab3dbe9fbd75c76d151ae58c416f6
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:35:07 +0530

    Update legt_bottom_triangle_star.java

--- feb_3week/legt_bottom_triangle_star.java
+++ feb_3week/legt_bottom_triangle_star.java
@@ -1,5 +1,18 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class legt_bottom_triangle_star {
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter numbers of line: ");
+		n=sc.nextInt();
+		for(int i=n; i>0; i--) {
+			for(int j=i; j>1; j--) {
+				System.out.print("* ");
+			}
+			System.out.println("* ");
+		}
+	}
 
 }
+
