2359ee00249420a8a1e00d2bd2ade441a236f7c1
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:37:56 +0530

    Update while_10to1.java

--- feb_3week/while_10to1.java
+++ feb_3week/while_10to1.java
@@ -1,5 +1,16 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class while_10to1 {
+	public static void main(String[] args) {
+//		 Write a program to print 10 to 1 using while loop
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter last number: ");
+		n=sc.nextInt();
+		while(n>0) {
+			System.out.println(n);
+			n--;
+		}
+	}
 
 }
