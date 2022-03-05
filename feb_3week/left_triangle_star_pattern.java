2941c8542821e6373b3b60cad021a7a0059eec42
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:34:09 +0530

    Update left_triangle_star_pattern.java

--- feb_3week/left_triangle_star_pattern.java
+++ feb_3week/left_triangle_star_pattern.java
@@ -1,5 +1,17 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class left_triangle_star_pattern {
+	public static void main(String[] args) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter numbers of line: ");
+		n=sc.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("* ");
+		}
+	}
 
 }
