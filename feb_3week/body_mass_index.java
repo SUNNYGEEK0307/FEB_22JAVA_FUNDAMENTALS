00baf52928148c2f99349e8c63bfe0b74b3c40dd
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:27:43 +0530

    Update body_mass_index.java

--- feb_3week/body_mass_index.java
+++ feb_3week/body_mass_index.java
@@ -1,5 +1,16 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class body_mass_index {
+	public static void main(String[] args) {
+//		Write a Java program to compute body mass index (BMI)
+		double w,h;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Weight(Kg): ");
+		w=sc.nextDouble();
+		System.out.print("Enter Height(m): ");
+		h=sc.nextDouble();
+		double bmi= w/(h*h);
+		System.out.print("Body Mass Index (BMI) is: "+bmi+" Kg/m2");
+	}	
 
 }
