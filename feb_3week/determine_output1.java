5f5cff89769aa66c59ac72c101d184ed9e29bd24
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:38:44 +0530

    Update determine_output1.java

--- feb_3week/determine_output1.java
+++ feb_3week/determine_output1.java
@@ -1,7 +1,7 @@
 package assignment_feb4week;
 import java.util.Scanner;
 public class determine_output1 {
-	public static void main(String[] Pratham) {
+	public static void main(String[] args) {
 		// Determine the output of the following code:
 		int a = 10;
 		if(++a==11 || ++a==12)
