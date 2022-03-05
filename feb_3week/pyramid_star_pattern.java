d3ee152ee95004f1550a340fb39bf9c59a44f3c7
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:36:02 +0530

    Update pyramid_star_pattern.java

--- feb_3week/pyramid_star_pattern.java
+++ feb_3week/pyramid_star_pattern.java
@@ -1,5 +1,23 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class pyramid_star_pattern {
+	public static void main(String[] args) {
+		int n, k=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter numbers of line: ");
+		n=sc.nextInt();
+		for (int i=1; i<=n; ++i, k=0) {
+		      for (int space=1; space<=n-i; ++space) {
+		        System.out.print("  ");
+		      }
+
+		      while (k!=2*i-1) {
+		        System.out.print("* ");
+		        ++k;
+		      }
+
+		      System.out.println();
+		}
+	}
 
 }
