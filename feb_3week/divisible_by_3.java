f47be523485cd181467095b198eab7d5a389c9ba
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:30:22 +0530

    Update divisible_by_3.java

--- feb_3week/divisible_by_3.java
+++ feb_3week/divisible_by_3.java
@@ -1,5 +1,17 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class divisible_by_3 {
+	public static void main(String[] Sunny) {
+		int n, i=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter lat number: ");
+		n=sc.nextInt();
+		while(i<n) {
+			if(i%3==0 && i!=0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+	}
 
 }
