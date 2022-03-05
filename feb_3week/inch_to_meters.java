2440790e01f9a3af445fd28bcca74ab42041c142
SUNNYGEEK0307 <100506285+SUNNYGEEK0307@users.noreply.github.com>
2022-03-05 19:33:19 +0530

    Update inch_to_meters.java

--- feb_3week/inch_to_meters.java
+++ feb_3week/inch_to_meters.java
@@ -1,5 +1,12 @@
 package assignment_feb3week;
-
+import java.util.Scanner;
 public class inch_to_meters {
-
+	public static void main(String[] args) {
+		double inch, meter= 0.0508;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter value(in inches): ");
+		inch=sc.nextDouble();
+		meter=meter*inch;
+		System.out.print(inch+" inches = "+meter+" m");
+	}
 }
